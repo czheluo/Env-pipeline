b7d2ed5fa81db3e7a97c60f85b05ed8e9ea24d15
Chenzhe Luo <czheluo@gmail.com>
2019-06-03 16:31:25 +0800

    Add files via upload

--- bin/bin/bayenv.r
+++ bin/bin/bayenv.r
@@ -1,4 +1,4 @@
-times<-Sys.time() 
+times<-Sys.time()
 library(qqman)
 library('getopt')
 options(bitmapType='cairo')
@@ -12,11 +12,11 @@ print_usage <- function(spec=NULL){
     cat(getopt(spec, usage=TRUE));
     cat("Usage example: \n")
     cat("
-    Usage example: 
+    Usage example:
     Usage:
     --infile sweep out file
     --outfile figure name
-    --table out file  
+    --table out file
     --help      usage
     \n")
     q(status=1);
@@ -26,17 +26,21 @@ if ( is.null(opt$infile) ) { print_usage(spec) }
 if ( is.null(opt$outfile) ) { print_usage(spec) }
 
 mydata<-read.table(opt$infile,header = TRUE,sep="\t")
-ymaxP=ceiling(max(mydata$P,na.rm=TRUE))
+mydata$ne <- log10(mydata[,3])
+newdata <- mydata[c(1,2,4)]
+colnames(newdata) <- c("CHR","POS","P")
+ymaxP=ceiling(max(newdata$P,na.rm=TRUE))
+yminP=ceiling(min(newdata$P,na.rm=TRUE))
 png(paste(opt$outfile,".png",sep=""),height=900,width=1600)
-manhattan(mydata,chr="CHR",bp="POS",p="P",col=rainbow(4),chrlabs=("SNPs"),ylab="log10(BF)",ylim=c(0,ymaxP),suggestiveline=FALSE ,genomewideline=FALSE,logp=FALSE)
+manhattan(newdata,chr="CHR",bp="POS",p="P",col=rainbow(4),chrlabs=("SNPs"),ylab="log10(BF)",ylim=c(yminP,ymaxP),suggestiveline=FALSE ,genomewideline=1.5,logp=FALSE)
 dev.off()
 pdf(paste(opt$outfile,".pdf",sep=""),height=8,width=16)
-manhattan(mydata,chr="CHR",bp="POS",p="P",col=rainbow(4),chrlabs=("SNPs"),ylab="log10(BF)",ylim=c(0,ymaxP),suggestiveline=FALSE,genomewideline=FALSE,logp=FALSE)
+manhattan(newdata,chr="CHR",bp="POS",p="P",col=rainbow(4),chrlabs=("SNPs"),ylab="log10(BF)",ylim=c(yminP,ymaxP),suggestiveline=FALSE,genomewideline=1.5,logp=FALSE)
 dev.off()
-select <- subset(mydata,log10(mydata$P)>=1.5)
-select <- select[with(select,order(mydata$POS)),]
-select <- select[order(mydata$POS,mydata$P),]
+select <- subset(newdata,newdata$P>=1.5)
+select <- select[with(select,order(newdata$POS)),]
+select <- select[order(newdata$POS,newdata$P),]
 write.table(select, file = opt$table)
 escaptime=Sys.time()-times;
 print("Done!")
-print(escaptime) 
+print(escaptime)
